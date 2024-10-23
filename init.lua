2c459d2cea0401df4d8c97f8d44fee190a09e7ef
Fabian Topfstedt <topfstedt@schneevonmorgen.com>
2024-10-23 10:19:27 +0200

    templ lsp

--- init.lua
+++ init.lua
@@ -526,6 +526,7 @@ require('lazy').setup({
         -- clangd = {},
         gopls = {},
         pyright = {},
+        templ = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
